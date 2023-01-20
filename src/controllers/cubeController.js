59b611be8af58a1acd9d1e41a3cdba80b05e505f
borisovch1 <Yvanov838@abv.bg>
2023-01-20 10:08:27 +0200

    Add details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 
 const Cube=require('../models/Cube')
+const db= require('../bd.json')
 
 exports.getCreateCube = (req, res) => {
     res.render('create')
@@ -14,4 +15,15 @@ exports.postCreateCube = (req, res) => {
     let cube = new Cube(name, description, imageUrl, difficultyLevel)
     Cube.save(cube)
     res.redirect('/')
+}
+exports.getDetails = (req, res) => {
+    let cubeId = Number(req.params.cubeId)
+    if (!cubeId) {
+        return res.redirect('404')
+    }
+    let cube = db.cubes.find(x => x.id === cubeId)
+    if (!cube) {
+        return res.redirect('404')
+    }
+    res.render('details',{cube})
 }
