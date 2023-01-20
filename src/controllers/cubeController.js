63e1a6ecf904fbcbadfc30fdf9e29ba89879a424
borisovch1 <Yvanov838@abv.bg>
2023-01-20 10:40:59 +0200

    Add search

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -19,11 +19,11 @@ exports.postCreateCube = (req, res) => {
 exports.getDetails = (req, res) => {
     let cubeId = Number(req.params.cubeId)
     if (!cubeId) {
-        return res.redirect('404')
+        return res.redirect('/404')
     }
     let cube = db.cubes.find(x => x.id === cubeId)
     if (!cube) {
-        return res.redirect('404')
+        return res.redirect('/404')
     }
     res.render('details',{cube})
 }
