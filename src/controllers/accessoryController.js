7a6bef58431798e9e22af05759741cb56ee731d1
borisovch1 <Yvanov838@abv.bg>
2023-01-27 10:37:58 +0200

    Add accessory page

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,8 +1,15 @@
 
 const router = require('express').Router()
+const Accessory=require('../models/Accessory')
 
 router.get('/create',(req,res)=>{
     res.render('accessory/create')// in folder accessory in file create
+})
+router.post('/create',async(req,res)=>{// create accessory in mongoDb
+    const { name, description, imageUrl } = req.body
+    
+    await Accessory.create({ name, description, imageUrl })
+    res.redirect('/')
 } )
 
 module.exports=router
